f5329d16ba73c9641ddb475b01af6b05246e6d24
Francois David <francois.da@hotmail.com>
2019-12-19 16:46:22 -0500

    comments clarifications

--- RefineLabelSpace.m
+++ RefineLabelSpace.m
@@ -1,3 +1,5 @@
+% Function used to refine the label space, used and called by the MRF main segment.
 function L = RefineLabelSpace(Lprev, refinementScalor)
-L = Lprev  .* refinementScalor;
-L = round(L);
+ % Multiply each values in the label space by the refinement scalor that is normally smaller than 1. 
+ L = Lprev  .* refinementScalor;
+ L = round(L);
