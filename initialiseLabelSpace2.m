d3b10bf8b96442387f65f9b44587901e1436058b
Francois David <francois.da@hotmail.com>
2019-12-19 18:40:15 -0500

    Update initialiseLabelSpace2.m

--- initialiseLabelSpace2.m
+++ initialiseLabelSpace2.m
@@ -1,10 +1,13 @@
-
-
+% Function used to initialise the label space depending on the max length of the vectors and the scaling. 
 function L=initialiseLabelSpace2(dmax, scale)
+% Start with the null vector. 
  L = [0 0];
  currentMax = dmax;
+ % iterate through all the scaling.
  for i = 1:scale
+      % Calculate the current max for this scaling. 
       currentMax = (dmax/scale) *i;
+      % Calculate the diagonal values. Rounded of course. 
       diag = round(currentMax * currentMax / sqrt(3* currentMax * currentMax));
        L = [L;
            currentMax 0;
@@ -16,8 +19,3 @@ function L=initialiseLabelSpace2(dmax, scale)
           -diag,diag;
          -diag,-diag];
  end
- 
- 
- 
- 
- L = currentMax;
