f19dca8566c06c75ad967fb545f65b765d07db1c
Francois David <francois.da@hotmail.com>
2019-12-19 16:44:03 -0500

    Comments on SAD

--- SAD.m
+++ SAD.m
@@ -1,9 +1,11 @@
+% Function used to calculate the Sum Absolute Difference between two images.
 function [sad] = SAD(I,J)
-
+% Initialize to zero.
 sad = 0 ;
-
+    % Iterate through each pixels of the image, in all of the dicom used, the size was 512 but it could be changed accordingly.
     for i = 1:512
         for j = 1:512
+            % Add the difference to the total sad variable.
             a = double(I(i,j));
             b = double(J(i,j));
             sad = sad + abs(a - b);
